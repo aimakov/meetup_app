77fd21bfb059aef5c521c84920340941f746fbde
AIMAKOVNURBOLAT <aimakov@unist.ac.kr>
2020-10-26 22:45:35 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Top Toolbar - Meetup types</div>
+      <div>Side Toolbar - Meetup options</div>
+      <div>Main Window - Setting up an event</div>
+    </div>
+  );
+}
+
+export default App;
